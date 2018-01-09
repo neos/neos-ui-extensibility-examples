7f47baaeffc536360b2d215486d44272c2e40a99
Dominique Feyer <dfeyer@ttree.ch>
2018-01-09 21:22:26 +0100

    TASK: Replace editorsRegistry.add by editorsRegistry.set

--- Resources/Private/CustomEditor/src/manifest.js
+++ Resources/Private/CustomEditor/src/manifest.js
@@ -5,7 +5,7 @@ import CustomTextField from './CustomTextField';
 manifest('Neos.Neos.Ui.ExtensibilityExamples:CustomEditor', {}, globalRegistry => {
     const editorsRegistry = globalRegistry.get('inspector').get('editors');
 
-    editorsRegistry.add('Neos.Neos/Inspector/Editors/TextFieldEditor', {
+    editorsRegistry.set('Neos.Neos/Inspector/Editors/TextFieldEditor', {
         component: CustomTextField
     });
 });
